ed043fa83ebf766d4ede08e9888545003ee7514f
Brandon Nott <brandon.nott@levvel.io>
2017-12-24 13:23:31 -0500

    added copy operation tests

--- Example/Tests/CopyOperationTests.swift
+++ Example/Tests/CopyOperationTests.swift
@@ -1,9 +1,69 @@
-//
-//  CopyOperationTest.swift
-//  JsonPatchSwift_Tests
-//
-//  Created by Brandon Nott on 12/24/17.
-//  Copyright © 2017 CocoaPods. All rights reserved.
-//
+import XCTest
+@testable import JsonPatchSwift
+import SwiftyJSON
 
-import Foundation
+// http://tools.ietf.org/html/rfc6902#section-4.5
+// 4.  Operations
+// 4.5. copy
+class JPSCopyOperationTests: XCTestCase {
+    
+    // reusable method
+    private func testPatchOperation(json jsonString: String, jsonPatch jsonPatchString: String, expectedJSON expectedJSONString: String) {
+        do {
+            let json = JSON(parseJSON: jsonString)
+            let jsonPatch = try JPSJsonPatch(jsonPatchString)
+            let resultingJSON = try JPSJsonPatcher.applyPatch(jsonPatch, toJson: json)
+            let expectedJSON = JSON(parseJSON: expectedJSONString)
+            XCTAssertEqual(resultingJSON, expectedJSON)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+    }
+    
+    func testIfCopyReplaceValueInObjectReturnsExpectedValue() {
+        let json = """
+        {"foo": {"1": 2}, "bar": {}}
+        """
+        let jsonPatch = """
+        {"op": "copy", "from": "/foo", "path": "/bar"}
+        """
+        let expectedJSON = """
+        {"foo": {"1": 2}, "bar": {"1": 2}}
+        """
+        testPatchOperation(json: json, jsonPatch: jsonPatch, expectedJSON: expectedJSON)
+    }
+    
+    func testIfCopyArrayReturnsExpectedValue() {
+        let json = """
+        {"foo": [1, 2, 3, 4], "bar": []}
+        """
+        let jsonPatch = """
+        {"op": "copy", "from": "/foo", "path": "/bar"}
+        """
+        let expectedJSON = """
+        {"foo": [1, 2, 3, 4], "bar": [1, 2, 3, 4]}
+        """
+        testPatchOperation(json: json, jsonPatch: jsonPatch, expectedJSON: expectedJSON)
+    }
+    
+    func testIfCopyArrayOfObjectsReturnsExpectedValue() {
+        let json = """
+        {"foo": [{"foo": "bar"}], "bar": {}}
+        """
+        let jsonPatch = """
+        {"op": "copy", "from": "/foo/0", "path": "/bar"}
+        """
+        let expectedJSON = """
+        {"foo": [{"foo": "bar"}], "bar": {"foo": "bar"}}
+        """
+        testPatchOperation(json: json, jsonPatch: jsonPatch, expectedJSON: expectedJSON)
+    }
+    
+    func testIfMissingParameterReturnsError() {
+        let jsonPatch = """
+        {"op": "copy", "path": "/bar"}
+        """
+        // missing "from"
+        XCTAssertThrowsError(try JPSJsonPatch(jsonPatch))
+    }
+}
