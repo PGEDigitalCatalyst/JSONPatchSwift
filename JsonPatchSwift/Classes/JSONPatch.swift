1bc8771d910e0d52ca6c086f89771b48e2f99c6b
Brandon Nott <brandon.nott@levvel.io>
2017-12-24 20:37:17 -0500

    checkin - cleanup

--- JsonPatchSwift/Classes/JSONPatch.swift
+++ JsonPatchSwift/Classes/JSONPatch.swift
@@ -1,69 +1,35 @@
+
 import SwiftyJSON
 
-/**
- Representation of a JSON Patch
- */
 public struct JSONPatch {
     
-    let operations: [JSONPatchOperation]
+    public let operations: [JSONPatchOperation]
     
-    /**
-     Initializes a new `JSONPatch` based on a given SwiftyJSON representation.
-     - Parameter _: A String representing one or many JSON Patch operations.
-     e.g. (1) JSON({ "op": "add", "path": "/", "value": "foo" })
-     or (> 1)
-     JSON([ { "op": "add", "path": "/", "value": "foo" },
-     { "op": "test", "path": "/", "value": "foo } ])
-     - Throws: can throw any error from `JSONPatch.JPSJsonPatchInitialisationError` to
-     notify failed initialization.
-     - Returns: a `JSONPatch` representation of the given SwiftJSON object
-     */
     public init(json: JSON) throws {
-        
-        // Check if there is an array of a dictionary as root element. Both are valid JSON patch documents.
-        if json.type == .dictionary {
-            self.operations = [try JSONPatch.extractOperationFromJson(json)]
-            
-        } else if json.type == .array {
-            guard 0 < json.count else {
-                throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.PatchWithEmptyError)
-            }
-            var operationArray = [JSONPatchOperation]()
-            for i in 0..<json.count {
-                let operation = json[i]
-                operationArray.append(try JSONPatch.extractOperationFromJson(operation))
-            }
-            self.operations = operationArray
-            
-        } else {
-            // All other types are not a valid JSON Patch Operation.
-            throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.InvalidRootElement)
+        switch json.type {
+        case .dictionary:
+            operations = [try JSONPatch.extractOperation(from: json)]
+        case .array:
+            guard !json.isEmpty else { throw JSONPatchError.emptyPatchArray }
+            operations = try json.array!.map({ try JSONPatch.extractOperation(from: $0) })
+        default:
+            throw JSONPatchError.invalidRootElement
         }
     }
     
-    /**
-     Initializes a new `JSONPatch` based on a given String representation.
-     - parameter _: A String representing one or many JSON Patch operations.
-     e.g. (1) { "op": "add", "path": "/", "value": "foo" }
-     or (> 1)
-     [ { "op": "add", "path": "/", "value": "foo" },
-     { "op": "test", "path": "/", "value": "foo } ]
-     - throws: can throw any error from `JSONPatch.JPSJsonPatchInitialisationError` to notify failed initialization.
-     - returns: a `JSONPatch` representation of the given JSON Patch String.
-     */
     public init(jsonString: String) throws {
-        let data = jsonString.data(using: .utf8)!
-        let json: JSON
-        do {
-            json = try JSON(data: data, options: .allowFragments)
-        } catch {
-            throw JPSJsonPatchInitialisationError.InvalidJsonFormat(message: error.localizedDescription)
-        }
+        guard let data = jsonString.data(using: .utf8) else { throw JSONPatchError.badStringEncoding }
+        let json = try JSON(data: data, options: .allowFragments)
         try self.init(json: json)
     }
     
-    /// Possible errors thrown by the init function.
-    public enum JPSJsonPatchInitialisationError: Error {
+    public enum JSONPatchError: Error {
+        case emptyPatchArray
+        case invalidRootElement
+        case badStringEncoding
+        case missingOperationElement
+        case missingPathElement
+        case invalidOperation
         /** InvalidJsonFormat: The given String is not a valid JSON. */
         case InvalidJsonFormat(message: String?)
         /** InvalidPatchFormat: The given Patch is invalid (e.g. missing mandatory parameters). See error message for details. */
@@ -87,24 +53,24 @@ extension JSONPatch: Equatable {
 // MARK: - Private functions
 extension JSONPatch {
     
-    private static func extractOperationFromJson(_ json: JSON) throws -> JSONPatchOperation {
+    private static func extractOperation(from json: JSON) throws -> JSONPatchOperation {
         
         // The elements 'op' and 'path' are mandatory.
         guard let operation = json[JPSConstants.JsonPatch.Parameter.Op].string else {
-            throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.OpElementNotFound)
+            throw JSONPatchError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.OpElementNotFound)
         }
         guard let path = json[JPSConstants.JsonPatch.Parameter.Path].string else {
-            throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.PathElementNotFound)
+            throw JSONPatchError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.PathElementNotFound)
         }
         guard let operationType = JSONPatchOperation.OperationType(rawValue: operation) else {
-            throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.InvalidOperation)
+            throw JSONPatchError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.InvalidOperation)
         }
         
         // 'from' element mandatory for .move, .copy operations
         var from: JSONPointer?
         if operationType == .move || operationType == .copy {
             guard let fromValue = json[JPSConstants.JsonPatch.Parameter.From].string else {
-                throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.FromElementNotFound)
+                throw JSONPatchError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.FromElementNotFound)
             }
             from = try JSONPointer(rawValue: fromValue)
         }
@@ -113,7 +79,7 @@ extension JSONPatch {
         let value = json[JPSConstants.JsonPatch.Parameter.Value]
         // counterintuitive null check: https://github.com/SwiftyJSON/SwiftyJSON/issues/205
         if (operationType == .add || operationType == .replace) && value.null != nil {
-            throw JPSJsonPatchInitialisationError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.ValueElementNotFound)
+            throw JSONPatchError.InvalidPatchFormat(message: JPSConstants.JsonPatch.InitialisationErrorMessages.ValueElementNotFound)
         }
         
         let pointer = try JSONPointer(rawValue: path)
--- JsonPatchSwift/Classes/JSONPatcher.swift
+++ JsonPatchSwift/Classes/JSONPatcher.swift
@@ -6,59 +6,45 @@ import SwiftyJSON
  */
 public struct JSONPatcher {
     
-    /**
-     Applies a given `JSONPatch` to a `JSON`.
-     - Parameter jsonPatch: the jsonPatch to apply
-     - Parameter json: the json to apply the patch to
-     - Throws: can throw any error from `JSONPatcher.JPSJsonPatcherApplyError` to
-     notify about failed operations.
-     - Returns: A new `JSON` containing the given `JSON` with the patch applied.
-     */
     public static func apply(patch: JSONPatch, to json: JSON) throws -> JSON {
         var tempJson = json
         for operation in patch.operations {
             switch operation.type {
-            case .add: tempJson = try JSONPatcher.add(operation, toJson: tempJson)
-            case .remove: tempJson = try JSONPatcher.remove(operation, toJson: tempJson)
-            case .replace: tempJson = try JSONPatcher.replace(operation, toJson: tempJson)
-            case .move: tempJson = try JSONPatcher.move(operation, toJson: tempJson)
-            case .copy: tempJson = try JSONPatcher.copy(operation, toJson: tempJson)
-            case .test: tempJson = try JSONPatcher.test(operation, toJson: tempJson)
+            case .add: tempJson = try JSONPatcher.add(operation, to: tempJson)
+            case .remove: tempJson = try JSONPatcher.remove(operation, to: tempJson)
+            case .replace: tempJson = try JSONPatcher.replace(operation, to: tempJson)
+            case .move: tempJson = try JSONPatcher.move(operation, to: tempJson)
+            case .copy: tempJson = try JSONPatcher.copy(operation, to: tempJson)
+            case .test: tempJson = try JSONPatcher.test(operation, to: tempJson)
             }
         }
         return tempJson
     }
     
     /// Possible errors thrown by the applyPatch function.
-    public enum JPSJsonPatcherApplyError: Error {
-        /** ValidationError: `test` operation did not succeed. At least one tested parameter does not match the expected result. */
-        case ValidationError(message: String?)
-        /** ArrayIndexOutOfBounds: tried to add an element to an array position > array size + 1. See: http://tools.ietf.org/html/rfc6902#section-4.1 */
-        case ArrayIndexOutOfBounds
-        /** InvalidJson: invalid `JSON` provided. */
-        case InvalidJson
+    public enum JSONPatcherError: Error {
+        /** validationError: `test` operation did not succeed. At least one tested parameter does not match the expected result. */
+        case validationError
+        /** arrayIndexOutOfBounds: tried to add an element to an array position > array size + 1. See: http://tools.ietf.org/html/rfc6902#section-4.1 */
+        case arrayIndexOutOfBounds
+        /** invalidJSON: invalid `JSON` provided. */
+        case invalidJSON
     }
 }
 
-
 // MARK: - Private functions
+
 extension JSONPatcher {
-    private static func add(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
-        
-        guard 0 < operation.pointer.pointerValue.count else {
-            return operation.value
-        }
-        
-        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) {
-            (traversedJson, pointer) -> JSON in
+    
+    private static func add(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
+        guard !operation.pointer.pointerValue.isEmpty else { return operation.value }
+        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) { (traversedJson, pointer) -> JSON in
             var newJson = traversedJson
             if var jsonAsDictionary = traversedJson.dictionaryObject, let key = pointer.pointerValue.first as? String {
                 jsonAsDictionary[key] = operation.value.object
                 newJson.object = jsonAsDictionary
             } else if var jsonAsArray = traversedJson.arrayObject, let indexString = pointer.pointerValue.first as? String, let index = Int(indexString) {
-                guard index <= jsonAsArray.count else {
-                    throw JPSJsonPatcherApplyError.ArrayIndexOutOfBounds
-                }
+                guard index <= jsonAsArray.count else { throw JSONPatcherError.arrayIndexOutOfBounds }
                 jsonAsArray.insert(operation.value.object, at: index)
                 newJson.object = jsonAsArray
             }
@@ -66,9 +52,8 @@ extension JSONPatcher {
         }
     }
     
-    private static func remove(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
-        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) {
-            (traversedJson: JSON, pointer: JSONPointer) in
+    private static func remove(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
+        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) { (traversedJson: JSON, pointer: JSONPointer) in
             var newJson = traversedJson
             if var dictionary = traversedJson.dictionaryObject, let key = pointer.pointerValue.first as? String {
                 dictionary.removeValue(forKey: key)
@@ -82,9 +67,8 @@ extension JSONPatcher {
         }
     }
     
-    private static func replace(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
-        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) {
-            (traversedJson: JSON, pointer: JSONPointer) in
+    private static func replace(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
+        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) { (traversedJson: JSON, pointer: JSONPointer) in
             var newJson = traversedJson
             if var dictionary = traversedJson.dictionaryObject, let key = pointer.pointerValue.first as? String {
                 dictionary[key] = operation.value.object
@@ -98,11 +82,9 @@ extension JSONPatcher {
         }
     }
     
-    private static func move(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
+    private static func move(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
         var resultJson = json
-        
-        try JSONPatcher.applyOperation(json, pointer: operation.from!) {
-            (traversedJson: JSON, pointer: JSONPointer) in
+        _ = try JSONPatcher.applyOperation(json, pointer: operation.from!) { (traversedJson: JSON, pointer: JSONPointer) in
             
             // From: http://tools.ietf.org/html/rfc6902#section-4.3
             //    This operation is functionally identical to a "remove" operation for
@@ -111,7 +93,7 @@ extension JSONPatcher {
             
             // remove
             let removeOperation = JSONPatchOperation(type: JSONPatchOperation.OperationType.remove, pointer: operation.from!, value: resultJson, from: operation.from)
-            resultJson = try JSONPatcher.remove(removeOperation, toJson: resultJson)
+            resultJson = try JSONPatcher.remove(removeOperation, to: resultJson)
             
             // add
             var jsonToAdd = traversedJson[pointer.pointerValue]
@@ -119,7 +101,7 @@ extension JSONPatcher {
                 jsonToAdd = traversedJson[index]
             }
             let addOperation = JSONPatchOperation(type: JSONPatchOperation.OperationType.add, pointer: operation.pointer, value: jsonToAdd, from: operation.from)
-            resultJson = try JSONPatcher.add(addOperation, toJson: resultJson)
+            resultJson = try JSONPatcher.add(addOperation, to: resultJson)
             
             return traversedJson
         }
@@ -127,17 +109,15 @@ extension JSONPatcher {
         return resultJson
     }
     
-    private static func copy(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
+    private static func copy(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
         var resultJson = json
-        
-        try JSONPatcher.applyOperation(json, pointer: operation.from!) {
-            (traversedJson: JSON, pointer: JSONPointer) in
+        _ = try JSONPatcher.applyOperation(json, pointer: operation.from!) { (traversedJson: JSON, pointer: JSONPointer) in
             var jsonToAdd = traversedJson[pointer.pointerValue]
             if traversedJson.type == .array, let indexString = pointer.pointerValue.first as? String, let index = Int(indexString) {
                 jsonToAdd = traversedJson[index]
             }
             let addOperation = JSONPatchOperation(type: JSONPatchOperation.OperationType.add, pointer: operation.pointer, value: jsonToAdd, from: operation.from)
-            resultJson = try JSONPatcher.add(addOperation, toJson: resultJson)
+            resultJson = try JSONPatcher.add(addOperation, to: resultJson)
             return traversedJson
         }
         
@@ -145,21 +125,16 @@ extension JSONPatcher {
         
     }
     
-    private static func test(_ operation: JSONPatchOperation, toJson json: JSON) throws -> JSON {
-        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) {
-            (traversedJson: JSON, pointer: JSONPointer) in
+    private static func test(_ operation: JSONPatchOperation, to json: JSON) throws -> JSON {
+        return try JSONPatcher.applyOperation(json, pointer: operation.pointer) { (traversedJson: JSON, pointer: JSONPointer) in
             let jsonToValidate = traversedJson[pointer.pointerValue]
-            guard jsonToValidate == operation.value else {
-                throw JPSJsonPatcherApplyError.ValidationError(message: JPSConstants.JsonPatch.ErrorMessages.ValidationError)
-            }
+            guard jsonToValidate == operation.value else { throw JSONPatcherError.validationError }
             return traversedJson
         }
     }
     
     private static func applyOperation(_ json: JSON?, pointer: JSONPointer, operation: ((JSON, JSONPointer) throws -> JSON)) throws -> JSON {
-        guard let newJson = json else {
-            throw JPSJsonPatcherApplyError.InvalidJson
-        }
+        guard let newJson = json else { throw JSONPatcherError.invalidJSON }
         if pointer.pointerValue.count == 1 {
             return try operation(newJson, pointer)
         } else {
@@ -174,5 +149,4 @@ extension JSONPatcher {
         }
         return newJson
     }
-    
 }
